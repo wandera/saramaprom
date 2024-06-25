edff33e505e2f4bcc8e86ae3ad99c524d406c535
tomas.svoboda <tomas.svoboda@jamf.com>
2024-06-25 10:03:35 +0200

    TRIVIAL: Update dependencies

--- saramaprom_test.go
+++ saramaprom_test.go
@@ -1,11 +1,11 @@
 package saramaprom_test
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	io_prometheus_client "github.com/prometheus/client_model/go"
 	"github.com/rcrowley/go-metrics"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -163,9 +163,40 @@ func TestSummary(t *testing.T) {
 
 	t.Run("check summary", func(t *testing.T) {
 		t.Log(metricFamilies[0].GetMetric()[0].GetSummary())
-		got := fmt.Sprint(metricFamilies[0])
-		want := `name:"test_subsys_metric" help:"metric" type:SUMMARY metric:<label:<name:"broker" value:"" > label:<name:"topic" value:"x" > summary:<sample_count:100 sample_sum:129 quantile:<quantile:0.05 value:1 > quantile:<quantile:0.1 value:1 > quantile:<quantile:0.25 value:1 > quantile:<quantile:0.5 value:1 > quantile:<quantile:0.75 value:1 > quantile:<quantile:0.9 value:1 > quantile:<quantile:0.95 value:5 > quantile:<quantile:0.99 value:9.949999999999974 > > > `
-		assert.Equal(t, want, got)
+
+		assert.Equal(t, "test_subsys_metric", metricFamilies[0].GetName())
+		assert.Equal(t, "metric", metricFamilies[0].GetHelp())
+		assert.Equal(t, io_prometheus_client.MetricType_SUMMARY, metricFamilies[0].GetType())
+
+		assert.Equal(t, []*io_prometheus_client.LabelPair{
+			ptr(io_prometheus_client.LabelPair{
+				Name:  ptr("broker"),
+				Value: ptr(""),
+			}), ptr(io_prometheus_client.LabelPair{
+				Name:  ptr("topic"),
+				Value: ptr("x"),
+			}),
+		}, metricFamilies[0].GetMetric()[0].GetLabel())
+
+		assert.Equal(t, uint64(100), metricFamilies[0].GetMetric()[0].GetSummary().GetSampleCount())
+		assert.Equal(t, float64(129), metricFamilies[0].GetMetric()[0].GetSummary().GetSampleSum())
+
+		assert.Equal(t, 0.05, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[0].GetQuantile())
+		assert.Equal(t, 1.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[0].GetValue())
+		assert.Equal(t, 0.1, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[1].GetQuantile())
+		assert.Equal(t, 1.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[1].GetValue())
+		assert.Equal(t, 0.25, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[2].GetQuantile())
+		assert.Equal(t, 1.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[2].GetValue())
+		assert.Equal(t, 0.5, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[3].GetQuantile())
+		assert.Equal(t, 1.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[3].GetValue())
+		assert.Equal(t, 0.75, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[4].GetQuantile())
+		assert.Equal(t, 1.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[4].GetValue())
+		assert.Equal(t, 0.9, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[5].GetQuantile())
+		assert.Equal(t, 1.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[5].GetValue())
+		assert.Equal(t, 0.95, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[6].GetQuantile())
+		assert.Equal(t, 5.0, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[6].GetValue())
+		assert.Equal(t, 0.99, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[7].GetQuantile())
+		assert.Equal(t, 9.949999999999974, metricFamilies[0].GetMetric()[0].GetSummary().GetQuantile()[7].GetValue())
 	})
 }
 
@@ -212,4 +243,8 @@ func getMetricDetails(pr *prometheus.Registry, fullName string) []gaugeDetails {
 		}
 	}
 	return nil
+}
+
+func ptr[T any](t T) *T {
+	return &t
 }
